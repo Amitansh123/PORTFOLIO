847d35b338872add3b3c39d048700183a6faa9e4
Amitansh123 <amitanshjohrirocks123@gmail.com>
2022-04-24 11:57:01 +0530

    modified some files

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("running")
+document.querySelector('.close').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if( document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.open').style.display = 'inline';  
+        document.querySelector('.close').style.display = 'none';  
+    }
+    else{
+        document.querySelector('.open').style.display = 'none'; 
+        setTimeout(()=>{
+            document.querySelector('.close').style.display = 'inline';  
+        },400)
+    }
+})
